d0cc0bba358f6851616997173ba19bc0d13e267e
elisabethduijnstee@gmail.com <elisabethduijnstee@gmail.com>
2023-05-01 19:16:57 +0100

    added sliders and y_hodl function

--- UniV3Analysis.py
+++ UniV3Analysis.py
@@ -2,25 +2,21 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 
-def calculate_pnl(spot, x, n_range):
-    pnl_data = []
-    for n in n_range:
-        lower = spot / n
-        upper = spot * n
-        spot_range = max(min(spot,upper),lower)
-        price_range = np.clip(x, lower, upper)
-        y = ((np.sqrt(price_range)) + x/np.sqrt(price_range) - (x/np.sqrt(spot_range)) - np.sqrt(spot_range)) / ((spot_range/np.sqrt(spot_range)) - np.sqrt(lower) + (np.sqrt(spot_range)) - (spot/np.sqrt(upper)))
-        pnl_data.append({'n': n, 'pnl': y})
-    return pnl_data
+def calculate_pnl(spot, x, lower, upper):
+    spot_range = max(min(spot,upper),lower)
+    price_range = np.clip(x, lower, upper)
+    y = ((np.sqrt(price_range)) + x/np.sqrt(price_range) - (x/np.sqrt(spot_range)) - np.sqrt(spot_range)) / ((spot_range/np.sqrt(spot_range)) - np.sqrt(lower) + (np.sqrt(spot_range)) - (spot/np.sqrt(upper)))
+    y_hold = ((x/np.sqrt(price_range)) - (x/np.sqrt(upper)) + np.sqrt(price_range) - np.sqrt(lower))/((spot/(np.sqrt(spot))) - (spot/np.sqrt(upper)) + np.sqrt(spot)-np.sqrt(lower)) -1 
+    return y, y_hold
 
-def plot_pnl(pnl_data):
+def plot_pnl(x, y, y_hold, lower, upper):
     fig, ax = plt.subplots()
-    for pnl in pnl_data:
-        ax.plot(x, pnl['pnl'], label=f'n: {pnl["n"]:.2f}')
+    ax.plot(x, y, label = 'y')
+    ax.plot(x, y_hold, label = 'y_hold')
     ax.set_xlabel('Price of Ethereum ($)')
     ax.set_ylabel('IL Rate')
     ax.set_xlim(0, 5000)
-    ax.set_ylim(-1.2, 0.2)
+    ax.set_ylim(-1.2, 1.2)
     ax.legend()
     ax.grid()
     st.pyplot(fig)
@@ -28,11 +24,10 @@ def plot_pnl(pnl_data):
 # Define the range of values for x
 x = np.linspace(0, 5000, 100)
 
-# Define the range of values for n
-n_range = [1.01, 1.10, 1.20, 1.50, 2.00, 10.00, np.inf]
-
 # Create a Streamlit app
 st.title('Ethereum Price vs Impermanent Loss')
-spot = st.slider('Select the spot price of Ethereum', min_value=0, max_value=5000, value=1500)
-pnl_data = calculate_pnl(spot, x, n_range)
-plot_pnl(pnl_data)
+spot = st.slider('Select the spot price of Ethereum', min_value=0.0, max_value=5000.0, value=1500.0, step=0.01)
+lower = st.slider('Select the lower price range', min_value=0.0, max_value=5000.0, value=1000.0, step=0.01)
+upper = st.slider('Select the upper price range', min_value=0.0, max_value=5000.0, value=2000.0, step=0.01)
+y, y_hold = calculate_pnl(spot, x, lower, upper)
+plot_pnl(x, y, y_hold, lower, upper)
